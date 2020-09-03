08cfae911b24aa8f0a95d7c52cf9b9ebf55c4a00
Shashikant Pathak <shashikantpathakk@gmail.com>
2020-09-03 21:21:46 +0200

    ui-start

--- app.js
+++ app.js
@@ -4,42 +4,26 @@ const stationStatusUrl = "https://gbfs.urbansharing.com/oslobysykkel.no/station_
 const header = { header: { "Client-Identifier": "mittfirma-reiseplanlegger" } }
 
 
-var list = []
-var availableCycle = []
 var resultJso = []
 var resultJso2 = []
-const stationList = fetch(stationListUrl, header).
-then((result) => {
-    return result.json();
-}).then((resultJson => {
-    resultJso = resultJson.data.stations
-        // list=resultJso.map((item)=>item.name)
-    fetch(stationStatusUrl, header)
-        .then((result2) => {
-            return result2.json()
-        }).then((resultJson2) => {
-            resultJso2 = resultJson2.data.stations
-            resultJso.slice(0, 5).map((item) => {
-                document.querySelector(".stationsList").innerHTML += `<div>
-                    <em>${item.name} </em>
-                    </div>`
-            })
-            resultJso2.slice(0, 5).map((item) => {
-                document.querySelector(".stationsList").innerHTML += `<div class="stationList" >
-                    <em>${item.num_bikes_available} </em>
-                    </div>`
-            })
+const stationList = fetch(stationListUrl, header)
+    .then((result) => result.json()
+        .then((resultJson => {
+            resultJso = resultJson.data.stations
+            fetch(stationStatusUrl, header)
+                .then(result2 => result2.json()
+                    .then((resultJson2) => {
+                        resultJso2 = resultJson2.data.stations
+                        let arr3 = resultJso.map((item, i) => Object.assign({}, item, resultJso2[i]));
+                        console.log(arr3);
+                        arr3.slice(0, 5).map((item) => {
+                            document.querySelector(".stationsList").innerHTML += `<div>
+                        <small>${item.name} </small>
+                        <small>${item.address} </small>
+                        <small>${item.num_bikes_available} </small>
+                         <small>${item.num_docks_available} </small>
+                        </div>`
+                        })
 
-        })
-}))
-
-
-// resultJso2.filter(o => resultJso.find((o2) => {
-//     if (o.station_id === o2.station_id) {
-//         // console.log(o.station_id, o2.station_id)
-//         document.querySelector("#stationsList").innerHTML += `<div>
-//         <em>${name} </em>
-//         <em>${item.address} </em>
-//         </div>`
-//     }
-// }))
+                    }))
+        })))
