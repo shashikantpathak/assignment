4eb73a34880f48941dea84e6ed91cb23f2cf4da8
Shashikant Pathak <shashikantpathakk@gmail.com>
2020-09-04 15:33:13 +0200

    completed

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const stationListUrl = "https://gbfs.urbansharing.com/oslobysykkel.no/station_information.json"
 const stationStatusUrl = "https://gbfs.urbansharing.com/oslobysykkel.no/station_status.json"
+const systemInformationUrl = "https://gbfs.urbansharing.com/oslobysykkel.no/system_information.json"
 
 const header = { header: { "Client-Identifier": "mittfirma-reiseplanlegger" } }
 
 
 var resultJso = []
 var resultJso2 = []
-const stationList = fetch(stationListUrl, header)
+var i = 0
+var arr3
+const stationList = () => fetch(stationListUrl, header)
     .then((result) => result.json()
         .then((resultJson => {
             resultJso = resultJson.data.stations
@@ -14,16 +17,49 @@ const stationList = fetch(stationListUrl, header)
                 .then(result2 => result2.json()
                     .then((resultJson2) => {
                         resultJso2 = resultJson2.data.stations
-                        let arr3 = resultJso.map((item, i) => Object.assign({}, item, resultJso2[i]));
+                        arr3 = resultJso.map((item, i) => Object.assign({}, item, resultJso2[i]));
                         console.log(arr3);
-                        arr3.slice(0, 5).map((item) => {
-                            document.querySelector(".stationsList").innerHTML += `<div>
-                        <small>${item.name} </small>
-                        <small>${item.address} </small>
-                        <small>${item.num_bikes_available} </small>
-                         <small>${item.num_docks_available} </small>
-                        </div>`
+                        arr3.slice(i, i + 6).map((item) => {
+                            document.querySelector(".cards").innerHTML += `
+                            <li class="cards__item">
+                                <div class="card">
+                                    <div class="card__image card__image--fence"></div>
+                                    <div class="card__content">
+                                        <div class="card__title">${item.name}</div>
+                                        <p class="card__text">${item.address}</p>
+                                        <button class="btn btn--block">
+                                        <div>Available-${item.num_bikes_available }</div>
+                                        <div> Free Space-${item.num_docks_available}</div>
+                                        </button>
+                                    </div>
+                                </div>
+                            </li>
+                          `
+
                         })
 
                     }))
         })))
+
+const systemInformation = fetch(systemInformationUrl, header)
+    .then(res => res.json())
+    .then(resJson => {
+        console.log(resJson.data)
+        document.querySelector(".service").textContent = resJson.data.name
+        document.querySelector(".operator").textContent = resJson.data.operator + "  " + resJson.data.phone_number
+
+
+    })
+
+document.querySelector(".load_button").addEventListener('click', function() {
+    i += 6
+    if (i <= arr3.length - 6) {
+        stationList()
+        console.log(i)
+    } else {
+        document.querySelector(".load_button").style.display = 'None'
+    }
+
+})
+
+stationList()
